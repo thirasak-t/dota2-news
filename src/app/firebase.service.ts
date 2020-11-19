53814183e1540a6fc96f66efe12e6de0fe953fd5
thirasak-t <nayzerotv@gmail.com>
2020-11-19 17:46:02 +0700

    1 file modified

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -4,7 +4,7 @@ import { AngularFireStorage } from "@angular/fire/storage";
 import * as firebase from "firebase/app";
 import { News } from "./news";
 import { Report } from "./report";
-import { Comment } from "./ comment";
+import { Comment } from "./comment";
 @Injectable({ providedIn: "root" })
 export class FirebaseService {
   constructor(private firestore: AngularFirestore) {}
@@ -14,7 +14,50 @@ export class FirebaseService {
     );
     return DocRef.valueChanges();
   }
-  addNews(n: string, t: string, image: string, ds:string) {
+  addReport(n: string, p: string) {
+    let report = {
+      topic: n,
+      problem: p,
+      date: firebase.default.firestore.Timestamp.now()
+    };
+    const ref = this.firestore.collection("report").add(report);
+    ref.then(newRef => {
+      const upDateID = {
+        id: newRef.id
+      };
+      newRef.update(upDateID);
+    });
+    return ref;
+  }
+  addComment(n: string, t: string, id_n: string) {
+    let comment = {
+      name: n,
+      text: t,
+      id_news: id_n,
+      date: firebase.default.firestore.Timestamp.now()
+    };
+    const ref = this.firestore.collection("comment").add(comment);
+    ref.then(newRef => {
+      const upDateID = {
+        id: newRef.id
+      };
+      newRef.update(upDateID);
+    });
+    return ref;
+  }
+  getComment() {
+    let DocRef = this.firestore.collection<Comment>("comment", e =>
+      e.orderBy("date", "desc")
+    );
+    return DocRef.valueChanges();
+  }
+  deletComment(id: string) {
+    return this.firestore
+      .collection("comment")
+      .doc(id)
+      .delete();
+  }
+  addNews(n: string, t: string, image: string, ds: string) {
     let news = {
       name: n,
       text: t,
